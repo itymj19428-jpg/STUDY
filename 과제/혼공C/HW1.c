2c4d0001d0e800fb73bc74d37bef6941e2a075c7
Minju <itymj19428@gmail.com>
2026-03-19 00:52:56 +0900

    INTEL 9기 혼공c HW1

---
+++ 과제/혼공C/HW1.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    double c = 300000.0;
+    double rate = 40e12;
+    double light_year = c * 365.0 * 24.0 * 60.0 * 60.0;
+    double vo_light_year = rate / light_year;
+
+    printf("보이저로 가려면 얼마의 시간이 걸리는가?[광년] : %.1lf\n", vo_light_year);
+
+    return 0;
+}
