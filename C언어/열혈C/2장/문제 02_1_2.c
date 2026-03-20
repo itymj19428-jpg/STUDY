6590d07bbd8ccea4c24ff4fd3af26051815e784c
Minju <itymj19428@gmail.com>
2026-03-21 02:04:39 +0900

    열혈C 2장

---
+++ C언어/열혈C/2장/문제 02_1_2.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    printf("이      름 : 윤성우\n");
+    printf("주      소 : 서울시 강서구 좋은동 123-3번지\n");
+    printf("전화번호 : 010-1122-3344\n");
+
+    return 0;
+}
