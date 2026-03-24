dbd39c8d62229a35f2ca49ecb30d167fbb382bd6
itymj19428-jpg <itymj19428@gmail.com>
2026-03-24 11:55:11 +0900

    Add files via upload

---
+++ C++/명품 C++/3장/예제 3-7.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle {
+public:
+	int radius;
+	Circle();
+	Circle(int r);
+	~Circle();
+	double getArea();
+};
+
+Circle::Circle() {
+	radius = 1;
+	cout << "반지름 " << radius << " 원 생성" << endl;
+}
+
+Circle::Circle(int r) {
+	radius = r;
+	cout << "반지름 " << radius << " 원 생성" << endl;
+}
+
+Circle::~Circle() {
+	cout << "반지름 " << radius << " 원 생성" << endl;
+}
+
+double Circle::getArea() {
+	return 3.14 * radius * radius;
+}
+
+/*전역 객체 생성*/
+Circle globalDonut(1000);
+Circle globalPizza(2000);
+
+/*지역 객체 생성*/
+void f() {
+	Circle fDonut(100);
+	Circle fPizza(200);
+}
+
+/*지역객체 생성*/
+int main()
+{
+	Circle mainDonut;
+	Circle mainPizza(30);
+	f();
+}
