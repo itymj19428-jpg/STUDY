28d33e6c5e1173925bac80ef352cc3626ae37a12
Minju <itymj19428@gmail.com>
2026-03-24 19:33:30 +0900

    혼공C 과제

---
+++ 과제/혼공C/HW2.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    double weight;
+    double height, height_m;
+    double BMI;
+
+    printf("몸무게(kg)와 키(cm) 입력 : ");
+    scanf("%lf%lf", &weight, &height);
+
+    height_m = height / 100.0;
+
+    BMI = (weight) / (height_m * height_m);
+
+    if ((BMI >= 20.0) && (BMI < 25.0))
+    {
+        printf("BMI : %.1lf. 표준입니다.\n", BMI);
+    }
+    else
+    {
+        printf("BMI : %.1lf. 체중관리가 필요합니다.\n", BMI);
+    }
+
+    return 0;
+}
