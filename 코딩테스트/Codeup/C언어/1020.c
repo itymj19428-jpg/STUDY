1ad850fab50f128b6e4b077e635a0f49313a2eb7
Minju <itymj19428@gmail.com>
2026-03-29 18:52:19 +0900

    C CORDING TEST

---
+++ 코딩테스트/Codeup/C언어/1020.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2;
+    char character;
+
+    scanf("%d%c%d", &num1, &character, &num2);
+
+    printf("%06d%06d\n", num1, num2);
+
+    return 0;
+}
