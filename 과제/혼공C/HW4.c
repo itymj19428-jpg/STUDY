32b86d410aac42944621c1c8b7a9bd90ccfc898b
Minju <itymj19428@gmail.com>
2026-03-29 22:13:23 +0900

    C STUDY

---
+++ 과제/혼공C/HW4.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    int count = 0;
+    int i, j;
+    int n_prime;
+
+    printf("2 이상의 정수를 입력하세요 : ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i++)
+    {
+        n_prime = 1;
+        for (j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                n_prime = 0;
+                break;
+            }
+        }
+        if (n_prime == 1)
+        {
+            printf("%5d", i);
+            count++;
+
+            if (count % 5 == 0)
+            {
+                printf("\n");
+            }
+        }
+    }
+
+    return 0;
+}
